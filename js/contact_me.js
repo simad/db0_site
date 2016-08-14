f4117ef38dfe3553a721b165f6177238ed7cb2f0
Silvia <sil.mandala@gmail.com>
2016-08-14 09:12:59 +0100

    Switch to formspree for handling contact form

--- js/contact_me.js
+++ js/contact_me.js
@@ -10,23 +10,34 @@ $(function() {
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
-            var phone = $("input#phone").val();
             var message = $("textarea#message").val();
+            var gotcha = $("input#_gotcha").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
+                url: "https://formspree.io/hello@divisionbyzero.co",
+                method: "POST",
                 data: {
                     name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
+                    _subject: "Div0 Form Submission",
+                    _replyto: email,
+                    message: message,
+                    _gotcha: gotcha
                 },
+                dataType: "json",
                 cache: false,
+                beforeSend: function() {
+                    $('#success').html("<div class='alert alert-loading'>");
+                    $('#success > .alert-loading').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-loading')
+                        .append("<strong>Sending message... </strong>");
+                    $('#success > .alert-loading')
+                        .append('</div>');
+                },
                 success: function() {
                     // Success message
                     $('#success').html("<div class='alert alert-success'>");
@@ -50,6 +61,7 @@ $(function() {
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
+                timeout: 3000
             })
         },
         filter: function() {
